be61fc63815bb02eac978bf59067c94a5d054d96
Pavel Markov <38042935+Markovpavel32@users.noreply.github.com>
2018-08-06 17:47:37 +0300

    Create fizzBuzz.js

---
+++ fizzBuzz.js
@@ -0,0 +1,15 @@
+const fizzBuzz = (begin, end) =>{
+  if(begin <= end){
+    for(let i = begin; i <= end; i++){
+      if(i % 3 === 0 && i % 5 === 0){
+        console.log('FizzBuzz');
+      } else if(i % 3 === 0){
+        console.log('Fizz');
+      } else if(i % 5 === 0){
+        console.log('Buzz');
+      } else{
+      console.log(i);}
+    }
+  }
+}
+export default fizzBuzz;
